7db2a3e31d4df3c90b53d56b1e195b427f144beb
andres-1891 <mauricio_acastro@soy.sena.edu.co>
2025-05-08 18:26:27 -0500

    se agregan funciones actualizar y mostrar

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from mostrar import mostrar_inventario
+from actualizar import actuazlizar_producto
+
 #inventario inicial
 inventario = {
     'manzanas': 50,
     'naranjas': 30,
     'peras':20
 }
+
+mostrar_inventario(inventario)
+actuazlizar_producto(inventario)
